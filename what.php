df6c23dabd7cd4e439b107bb15bec76eb715e1a7
Jason A. Clark <jasclark@gmail.com>
2016-08-05 23:37:38 -0400

    moved script src to closing <body>, set <link> tag as customCSS to replace @import

--- what.php
+++ what.php
@@ -13,21 +13,11 @@ $customScript[0] = './meta/scripts/global.js';
 <title><?php echo($pageTitle); ?> : Montana State University Libraries</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="alternate" type="application/rss+xml" title="MSU Libraries: Tools" href="http://feeds.feedburner.com/msulibrarySpotlightTools" />
-<style type="text/css" media="screen, projection, handheld">
-<!-- @import url("./meta/styles/default.css"); -->
-<!--
-<?php if ($customCSS != 'none') {
-	echo '@import url("'.dirname($_SERVER['PHP_SELF']).'/meta/styles/'.$customCSS.'");'."\n";
-}
-?>
--->
-</style>
 <?php
-if ($customScript) {
-  $counted = count($customScript);
-  for ($i = 0; $i < $counted; $i++) {
-   echo '<script type="text/javascript" src="'.$customScript[$i].'"></script>'."\n";
-  }
+if ($customCSS != 'none') {
+?>
+<link href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/meta/styles/<?php echo $customCSS; ?>" rel="stylesheet"/>
+<?php
 }
 ?>
 </head>
@@ -44,5 +34,13 @@ if ($customScript) {
 		<p>This is a proof of concept app and teaching demo. I wanted to illustrate how a location-based browse/search app for Worldcat data could engage patrons. Built with PHP, JavaScript, HTML, Open Library API for thumbnails, Google Ajax Search API for Geolocation, and Worldcat Basic Search API.</p>
 	</div><!-- end div main -->
 </div><!-- end container div -->
+<?php
+if ($customScript) {
+  $counted = count($customScript);
+  for ($i = 0; $i < $counted; $i++) {
+   echo '<script type="text/javascript" src="'.$customScript[$i].'"></script>'."\n";
+  }
+}
+?>
 </body>
 </html>
