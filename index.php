f2613207d83d33a64a8767ba491158c6510c90f4
jasonclark <jasclark@gmail.com>
2016-11-26 19:28:04 -0500

    testing dns-prefetch for performance

--- index.php
+++ index.php
@@ -47,6 +47,8 @@ $customScript[0] = './meta/scripts/global.js';
 <meta name="twitter:creator" property="og:site_name" content="@jaclark"/>
 <meta name="twitter:card" content="summary_large_image"/>
 <meta name="twitter:site" content="http://www.jasonclark.info"/>
+<meta http-equiv="x-dns-prefetch-control" content="on"/>
+<link rel="dns-prefetch" href="//fonts.googleapis.com"/>
 <link rel="alternate" type="application/rss+xml" title="MSU Libraries: Tools" href="http://feeds.feedburner.com/msulibrarySpotlightTools" />
 <link rel="canonical" href="<?php echo $protocol.$server.$path.'/'.$fileName; ?>"/>
 <?php
